c4a52e2d1f6fc43bd37f653f20c58358f00ddc50
justin <justin.wang4@icloud.com>
2021-06-23 15:26:38 +0800

    push dist

---
+++ dist/precache-manifest.0964018c4c7180034aa320731384d1a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f462d91e4a33349fe4",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "93065993dfeb029c8e40d34f572cf15c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "84f462d91e4a33349fe4",
+    "url": "/js/app.8417e3c3.js"
+  },
+  {
+    "revision": "f922657816b61b2f881b",
+    "url": "/js/chunk-vendors.d7a17b53.js"
+  },
+  {
+    "revision": "2ba5a6e01ff76eccb79cf6764a94a9fb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
