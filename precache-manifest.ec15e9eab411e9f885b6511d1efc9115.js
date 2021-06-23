9029856d340be715360e1510e54208f8fcfb65d7
justin <justin.wang4@icloud.com>
2021-06-23 18:01:50 +0800

    test

---
+++ precache-manifest.ec15e9eab411e9f885b6511d1efc9115.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3c1bbd589fc23d8e4f2",
+    "url": "/pwa-demo/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-demo/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "da2107b83cb908ffd740e0dad7c338a5",
+    "url": "/pwa-demo/index.html"
+  },
+  {
+    "revision": "c3c1bbd589fc23d8e4f2",
+    "url": "/pwa-demo/js/app.e2c960d6.js"
+  },
+  {
+    "revision": "f922657816b61b2f881b",
+    "url": "/pwa-demo/js/chunk-vendors.d7a17b53.js"
+  },
+  {
+    "revision": "2ba5a6e01ff76eccb79cf6764a94a9fb",
+    "url": "/pwa-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-demo/robots.txt"
+  }
+]);
